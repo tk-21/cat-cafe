8dae305f3c947f2a87bcb880e88588135d25e006
tk <83660733+tk-21@users.noreply.github.com>
2022-09-29 23:51:23 +0900

    お問い合わせフォーム

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+
+//お問い合わせフォーム
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');

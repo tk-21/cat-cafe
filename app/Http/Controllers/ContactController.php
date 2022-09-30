8f651c07b4ec5e77b33dbe6e2960470c09af28ac
tk <83660733+tk-21@users.noreply.github.com>
2022-09-30 23:05:51 +0900

    エラーメッセージの表示

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ContactController extends Controller
 {
@@ -10,6 +11,22 @@ class ContactController extends Controller
     {
         return view('contact.index');
     }
+
+    public function sendMail(Request $request) {
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'name_kana' => ['required', 'string', 'max:255', 'regex:/^[ァ-ロワンヴー]*$/u'],
+            'phone' => ['nullable', 'regex:/^0(\d-?\d{4}|\d{2}-?\d{3}|\d{3}-?\d{2}|\d{4}-?\d|\d0-?\d{4})-?\d{4}$/'],
+            'email' => ['required', 'email'],
+            'body' => ['required', 'string', 'max:2000'],
+        ]);
+
+        // これ以降の行は入力エラーがなかった場合のみ実行されます
+        // 登録処理(実際はメール送信などを行う)
+        Log::debug($validated['name']. 'さんよりお問い合わせがありました');
+        return to_route('contact.complete');
+    }
+
 
     public function complete()
     {
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/', function () {
 
 //お問い合わせフォーム
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::post('/contact', [ContactController::class, 'sendMail']);
 Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');

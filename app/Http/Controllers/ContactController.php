649d1d546eac0e0fe13a6701e62a5ef3b55a32c8
tk <83660733+tk-21@users.noreply.github.com>
2022-10-01 17:54:31 +0900

    フォームリクエスト

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -12,14 +13,8 @@ class ContactController extends Controller
         return view('contact.index');
     }
 
-    public function sendMail(Request $request) {
-        $validated = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'name_kana' => ['required', 'string', 'max:255', 'regex:/^[ァ-ロワンヴー]*$/u'],
-            'phone' => ['nullable', 'regex:/^0(\d-?\d{4}|\d{2}-?\d{3}|\d{3}-?\d{2}|\d{4}-?\d|\d0-?\d{4})-?\d{4}$/'],
-            'email' => ['required', 'email'],
-            'body' => ['required', 'string', 'max:2000'],
-        ]);
+    public function sendMail(ContactRequest $request) {
+        $validated = $request->validated();
 
         // これ以降の行は入力エラーがなかった場合のみ実行されます
         // 登録処理(実際はメール送信などを行う)

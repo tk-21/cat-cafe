a03cdfe8cc7e301355eb20added009c6c7e266c8
tk <83660733+tk-21@users.noreply.github.com>
2022-10-09 12:34:50 +0900

    ルートグループ

--- routes/web.php
+++ routes/web.php
@@ -27,22 +27,42 @@ Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::post('/contact', [ContactController::class, 'sendMail']);
 Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
 
-//ブログ
+//管理画面
+Route::prefix('/admin')
+    ->name('admin.')
+    ->group(function () {
+//ログイン時のみアクセス可能なルート
 //ログインしていなかったらauthミドルウェアでログイン画面へ遷移させる
-Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name('admin.blogs.index')->middleware('auth');
-Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('admin.blogs.create');
-Route::post('/admin/blogs', [AdminBlogController::class, 'store'])->name('admin.blogs.store');
-Route::get('/admin/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit')->middleware('auth');
-Route::put('/admin/blogs/{blog}', [AdminBlogController::class, 'update'])->name('admin.blogs.update');
-Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('admin.blogs.destroy');
+        Route::middleware('auth')
+            ->group(function () {
+//ブログ
+//Route::get('/blogs', [AdminBlogController::class, 'index'])->name('blogs.index');
+//Route::get('/blogs/create', [AdminBlogController::class, 'create'])->name('blogs.create');
+//Route::post('/blogs', [AdminBlogController::class, 'store'])->name('blogs.store');
+//Route::get('/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('blogs.edit');
+//Route::put('/blogs/{blog}', [AdminBlogController::class, 'update'])->name('blogs.update');
+//Route::delete('/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('blogs.destroy');
+
+//ブログ
+                Route::resource('/blogs', AdminBlogController::class)->except('show');
+
+//ログアウトは必ずPOSTメソッドで行う
+                Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+            });
+
+//未ログイン時のみアクセス可能なルート
+        Route::middleware('guest')
+            ->group(function () {
+//認証
+//すでにログインしている状態でログインページへアクセスすると、guestミドルウェアでブログ一覧ページへ遷移させるようにする
+                Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+                Route::post('/login', [AuthController::class, 'login']);
+
+            });
+    });
 
 //ユーザー管理
 Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
 Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
 
-//認証
-//すでにログインしている状態でログインページへアクセスすると、guestミドルウェアでブログ一覧ページへ遷移させるようにする
-Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login')->middleware('guest');
-Route::post('/admin/login', [AuthController::class, 'login']);
-//ログアウトは必ずPOSTメソッドで行う
-Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
